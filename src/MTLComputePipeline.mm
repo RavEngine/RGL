467a2aa18fc004389d92af2f9cc041c0fddbd1c3
Ravbug <ravbuganimations@gmail.com>
2023-04-07 19:56:12 -0400

    Metal: create compute pipeline

--- src/MTLComputePipeline.mm
+++ src/MTLComputePipeline.mm
@@ -2,10 +2,14 @@
 #import <Metal/Metal.h>
 #include "MTLComputePipeline.hpp"
 #include "MTLDevice.hpp"
+#include "MTLShaderLibrary.hpp"
+#include "RGLMTL.hpp"
 
 namespace RGL{
     ComputePipelineMTL::ComputePipelineMTL(decltype(owningDevice) owningDevice, const RGL::ComputePipelineDescriptor &desc) : owningDevice(owningDevice) {
         
+        auto shadermodule = desc.stage.shaderModule;
+        MTL_CHECK(pipelineState = [owningDevice->device newComputePipelineStateWithFunction:shadermodule->function error:&err]);
     }
     ComputePipelineMTL::~ComputePipelineMTL() {
     
