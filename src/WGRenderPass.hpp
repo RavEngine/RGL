df47da3fe0e416911965216e611884d7bfa8bb34
Ravbug <ravbuganimations@gmail.com>
2023-10-17 16:39:39 -0400

    WebGPU: Begin implementing render pass

--- src/WGRenderPass.hpp
+++ src/WGRenderPass.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include <RGL/RenderPass.hpp>
+#include <array>
+#include <emscripten/html5_webgpu.h>
 
 namespace RGL{
     struct ITexture;
+    
     struct RenderPassWG : public IRenderPass{
+
+        WGPURenderPassDescriptor renderPass;
+        std::vector<WGPURenderPassColorAttachment> colorAttachments;
+        WGPURenderPassDepthStencilAttachment depthStencilAttachment;
+
+        // IRenderPass
         RenderPassWG(const RenderPassConfig& config);
         virtual ~RenderPassWG(){}
         void SetAttachmentTexture(uint32_t index, ITexture* texture) final;
