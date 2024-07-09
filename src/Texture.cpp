5e4a7a192758d145801be69ffb4e5fa964bd5567
Ravbug <ravbug@noreply.users.github.com>
2024-07-08 20:51:38 -0700

    Gate based on backends enabled

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -7,10 +7,14 @@ namespace RGL {
 	uint32_t TextureView::GetReadonlyBindlessTextureHandle() {
 		auto backend = RGL::CurrentAPI();
 		switch (backend) {
+#if RGL_DX12_AVAILABLE
 		case API::Direct3D12:
 			return texture.dx.srvIDX;
+#endif
+#if RGL_VK_AVAILABLE
 		case API::Vulkan:
 			return static_cast<const TextureVk*>(parent)->globalDescriptorIndex;
+#endif
 		default:
 			throw std::runtime_error("Current backend does not support bindless texturing.");
 		}
